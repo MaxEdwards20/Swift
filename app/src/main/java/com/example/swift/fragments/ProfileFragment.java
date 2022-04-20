05aa9ee2c8382197917c00d92f1d0c10ae54c520
medwards <a02276129@usu.edu>
2022-04-20 12:13:53 -0600

    Secondary color is now the swift launcher background (blue)

---
+++ app/src/main/java/com/example/swift/fragments/ProfileFragment.java
@@ -0,0 +1,9 @@
+package com.example.swift.fragments;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.swift.R;
+
+public class ProfileFragment extends Fragment {
+    public ProfileFragment(){super(R.layout.fragment_profile);}
+}
